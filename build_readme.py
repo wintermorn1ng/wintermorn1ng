1f8669d5e53b41e4d33ab9f249aad42c12799ac3
wintermorn1ng <helloworldchen@qq.com>
2021-04-16 10:32:56 +0800

    Create build_readme.py

---
+++ build_readme.py
@@ -0,0 +1,83 @@
+from python_graphql_client import GraphqlClient
+import feedparser
+import httpx
+import json
+import pathlib
+import re
+import os
+import datetime
+
+root = pathlib.Path(__file__).parent.resolve()
+client = GraphqlClient(endpoint="https://api.github.com/graphql")
+
+
+TOKEN = os.environ.get("GH_TOKEN", "")
+
+
+def replace_chunk(content, marker, chunk, inline=False):
+    r = re.compile(
+        r"<!\-\- {} starts \-\->.*<!\-\- {} ends \-\->".format(marker, marker),
+        re.DOTALL,
+    )
+    if not inline:
+        chunk = "\n{}\n".format(chunk)
+    chunk = "<!-- {} starts -->{}<!-- {} ends -->".format(marker, chunk, marker)
+    return r.sub(chunk, content)
+
+def formatGMTime(timestamp):
+    GMT_FORMAT = '%a, %d %b %Y %H:%M:%S GMT'
+    dateStr = datetime.datetime.strptime(timestamp, GMT_FORMAT) + datetime.timedelta(hours=8)
+    return dateStr.date()
+
+def make_query(after_cursor=None):
+    return """
+query {
+  viewer {
+    repositories(first: 100, privacy: PUBLIC, after:AFTER) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      nodes {
+        name
+        description
+        url
+        releases(last:1) {
+          totalCount
+          nodes {
+            name
+            publishedAt
+            url
+          }
+        }
+      }
+    }
+  }
+}
+""".replace(
+        "AFTER", '"{}"'.format(after_cursor) if after_cursor else "null"
+    )
+
+
+def fetch_blog_entries():
+    entries = feedparser.parse("https://tw93.github.io/feed.xml")["entries"]
+    return [
+        {
+            "title": entry["title"],
+            "url": entry["link"].split("#")[0],
+            "published": entry["published"].split("T")[0],
+        }
+        for entry in entries
+    ]
+
+
+if __name__ == "__main__":
+    readme = root / "README.md"
+    readme_contents = readme.open().read()
+
+    entries = fetch_blog_entries()[:5]
+    entries_md = "\n".join(
+        ["* <a href='{url}' target='_blank'>{title}</a> - {published}".format(**entry) for entry in entries]
+    )
+    rewritten = replace_chunk(rewritten, "blog", entries_md)
+    readme.open("w").write(rewritten)
